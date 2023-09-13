9fedfe9b4fca294d99dd7a10a12b0cb2f4ba4aef
Noah Salmon <noahsalmon98@gmail.com>
2023-09-13 19:35:22 -0400

    define columns in product.js

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,40 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true
+      }
+    },
+
+    category_id: {
+      type: DataTypes.INTEGER,
+      foreignKey: {
+        name: 'category_id'//add a foreign key to reference the Category model's id
+      }
+      //TODO define columns 
+    },
   },
   {
     sequelize,
