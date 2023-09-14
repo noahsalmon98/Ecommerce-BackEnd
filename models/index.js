2d0e23b96604a4f858b167992dbcca611efbe518
Noah Salmon <noahsalmon98@gmail.com>
2023-09-13 20:17:29 -0400

    define model relationships in index.js

--- models/index.js
+++ models/index.js
@@ -4,13 +4,19 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+Category.hasMany(Product, {
+  foreignKey:'category_id',
+  onDelete: 'SET NULL'
+});
 
-// Categories have many Products
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+})
 
-// Products belongToMany Tags (through ProductTag)
 
-// Tags belongToMany Products (through ProductTag)
+Product.belongsToMany(Tag, {through: ProductTag});
+Tag.belongsToMany(Product,{through: ProductTag});
+
 
 module.exports = {
   Product,
