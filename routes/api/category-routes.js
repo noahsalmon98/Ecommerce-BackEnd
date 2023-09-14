f0a2abd2ba1f71db73b04d2afa2692ca73669a1c
Noah Salmon <noahsalmon98@gmail.com>
2023-09-13 21:19:27 -0400

    added put route to category-routes.js

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -36,20 +36,31 @@ router.get('/:id', async (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new category
-  Category.create(req.body).then(categoryData => 
-    res.json(categoryData)).catch(err => {
+  Category.create(req.body)
+  .then(categoryData => 
+    res.json(categoryData))
+    .catch(err => {
     res.json(err);
   })
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
-
+  Category.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  }).then((category) => {
+    res.json(category);
+  }
+  ).catch((err) =>
+    res.json(err));
 });
+
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
-  
+
 });
 
 module.exports = router;
